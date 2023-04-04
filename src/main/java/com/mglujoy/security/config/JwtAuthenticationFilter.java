88694eb047bac3601a23219c9f124895bc45acfd
mglujoy <mglujoy@gmail.com>
2023-04-04 19:13:55 -0300

    working on Authentication Filter --MG

--- src/main/java/com/mglujoy/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/mglujoy/security/config/JwtAuthenticationFilter.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mglujoy.security.config;
 
-/**
- *
- * @author mg
- */
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, 
+            HttpServletResponse response, 
+            FilterChain filterChain) 
+            throws ServletException, IOException {
+        ; 
+    }
     
 }

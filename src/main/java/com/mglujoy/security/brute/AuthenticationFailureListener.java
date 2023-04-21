9cb7e31962eef51428855bacbf1d910936ab11c8
mglujoy <mglujoy@gmail.com>
2023-04-21 12:15:54 -0300

    Final Commit --MG

---
+++ src/main/java/com/mglujoy/security/brute/AuthenticationFailureListener.java
@@ -0,0 +1,28 @@
+package com.mglujoy.security.brute;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFailureListener implements 
+  ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
+        final String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null || xfHeader.isEmpty() || !xfHeader.contains(request.getRemoteAddr())) {
+            loginAttemptService.loginFailed(request.getRemoteAddr());
+        } else {
+            loginAttemptService.loginFailed(xfHeader.split(",")[0]);
+        }
+    }
+}
